50772b72bead3b1c17e47a65a8ee2f1e3658a83c
francoisp75 <89409159+francoisp75@users.noreply.github.com>
2025-08-29 23:08:23 +0200

    update Home

--- Home.py
+++ Home.py
@@ -9,9 +9,10 @@ Bienvenue dans ce tableau de bord digital.
 
 Utilisez le **menu à gauche** pour naviguer entre les indicateurs clés :
 - **Page 1** : Revenu
-- **Page 2** : EBITDAaL
-- **Page 3** : Capex
-- **Page 4** : OPCF
+- **Page 2** : Revenu Europe
+- **Page 3** : EBITDAaL
+- **Page 4** : Capex
+- **Page 5** : OPCF
 
 Les données affichées couvrent les périodes de **2Q24 à 2Q25** et sont présentées par **opérateur**, **zone géographique** et **type de KPI**.
 """)
