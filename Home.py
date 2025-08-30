cd1c90fecb8e9e88b3ee5fdc7f61162a0310398a
francoisp75 <89409159+francoisp75@users.noreply.github.com>
2025-08-30 21:23:42 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,11 +8,13 @@ Bienvenue dans ce tableau de bord digital.
 **Comment Orange se positionne vs ses pairs sur les KPI financiers?**
 
 Utilisez le **menu à gauche** pour naviguer entre les indicateurs clés :
-- **Page 1** : Revenu
-- **Page 2** : Revenu Europe
-- **Page 3** : EBITDAaL
-- **Page 4** : Capex
-- **Page 5** : OPCF
+- **Page 1** : Top13
+- **Page 2** : Revenu
+- **Page 3** : Revenu Europe
+- **Page 4** : EBITDAaL
+- **Page 5** : Capex
+- **Page 6** : OPCF
+- **Page 7** : Market Cap
 
 Les données affichées couvrent les périodes de **2Q24 à 2Q25** et sont présentées par **opérateur**, **zone géographique** et **type de KPI**.
 """)
